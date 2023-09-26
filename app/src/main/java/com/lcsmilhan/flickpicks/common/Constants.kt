ac03f4c0799a489629efa71d41ecb5510aeb5da6
Lucas Milhan <lucasmilhan99@gmail.com>
2023-09-25 22:14:43 -0300

    Develop

---
+++ app/src/main/java/com/lcsmilhan/flickpicks/common/Constants.kt
@@ -0,0 +1,9 @@
+package com.lcsmilhan.flickpicks.common
+
+import com.lcsmilhan.flickpicks.BuildConfig
+
+object Constants {
+    const val TMDB_API_KEY = BuildConfig.TMDB_API_KEY
+    const val YT_API_KEY = BuildConfig.YT_API_KEY
+
+}
